514a8da158c08bd58584d7a780555b8a89f1dc07
Reduzer <83287196+Reduzer@users.noreply.github.com>
2023-08-29 12:37:30 +0200

    29/08/2023/12:37/
    Pac-Man Grundramen Erstellen

---
+++ PacMan/ghost.cs
@@ -0,0 +1,15 @@
+﻿namespace PacMan
+{
+    public class ghost
+    {
+        private int posX;
+        private int posY;
+
+        private int stateAgression;
+
+        public int getPosX()
+        {
+            return posX;
+        }
+    }
+}
---
+++ PacMan/player.cs
@@ -0,0 +1,13 @@
+﻿namespace PacMan
+{
+    public class player
+    {
+        private int lifes;
+        private int score;
+
+        private int posX;
+        private int posY;
+        
+        
+    }
+}
