ab2957648178200148b07b39fa08b79971ace19f
Fyn <83287196+Reduzer@users.noreply.github.com>
2023-09-20 08:18:00 +0200

    Add files via upload

---
+++ GameLogic/gamefunctions.cs
@@ -0,0 +1,29 @@
+﻿namespace Pac_Man
+{
+    public class gamefunctions
+    {
+        public void collisionDetection(int playerPosX, int playerPosY, int ghostX, int ghostY)
+        {
+            if ((playerPosX == ghostX) && (playerPosY == ghostY))
+            {
+                
+            }
+        }
+
+        public void gamestart()
+        {
+            
+        }
+
+        public void gameend()
+        {
+            
+        }
+
+        public void mapchanges()
+        {
+            
+            
+        }
+    }
+}
---
+++ GameLogic/ghost.cs
@@ -0,0 +1,43 @@
+﻿namespace Pac_Man
+{
+    public class ghost
+    {
+        private int posX;
+        private int posY;
+
+        private int speed;
+        private int state;
+        
+        public void move(int posPlayerX, int posPlayerY)
+        {
+            if (posPlayerX > posX)
+            {
+                    //try to move up;   
+            }
+
+            if (posPlayerX < posX)
+            {
+                //try to move down
+            }
+
+            if (posPlayerY > posY)
+            {
+                // try to move right
+            }
+            if (posPlayerY < posY)
+            {
+                // try to move left
+            }
+        }
+
+        public int getPosX()
+        {
+            return posX;
+        }
+
+        public int getPosY()
+        {
+            return posY;
+        }
+    }
+}
